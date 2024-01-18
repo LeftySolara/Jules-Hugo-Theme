f63fdb13e1b4ed42a14aad1e90d3c166801dcb90
Julianne Adams <julianne@julianneadams.dev>
2024-01-18 05:25:02 -0500

    Refactor: change theme toggle icon and color when theme changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
+/**
+ * Set the icon and color for the theme toggle button.
+ */
+export function setThemeToggleButton(currentTheme) {
+  const themeToggleButton = document.getElementById("theme-toggle");
+  if (!themeToggleButton) {
+    return;
+  }
+
+  icon = currentTheme == "light" ? "basil:sun-solid" : "basil:moon-solid";
+
+  themeToggleButton.className = `theme-toggle-${currentTheme}`;
+  themeToggleButton.setAttribute("icon", icon);
+}
+
 export function setThemeMode(currentTheme) {
-  document.body.className = `${currentTheme}-theme`;
+  if (document.body) {
+    document.body.className = `${currentTheme}-theme`;
+  }
+  setThemeToggleButton(currentTheme);
   localStorage.setItem("current-theme", `${currentTheme}`);
 }
 
